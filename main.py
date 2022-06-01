0e449621b6dada1fa4d0ebd32c74189ef781ac26
Ruan Castro <ruancastro15@gmail.com>
2022-06-01 20:27:02 -0300

    codigo para calcular a média de três valores

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("olá git, estou voltando a trabalhar com você (=, modificando novamente")
+valor = 0
+for i in range(0,3):
+    valor += float(input("digite um valor"))
+media = valor/3
+print(media)
