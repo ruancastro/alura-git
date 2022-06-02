8ad67f0c48a876c0b0729ce466c5e4f794ce6f07
Ruan Castro <ruancastro15@gmail.com>
2022-06-02 11:02:45 -0300

    mudei a print da segunda linha, treinando rebase aqui na branch titulo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 valor = 0
-print("Esse código é para calcular a média de três valores diferentes")
+print("treinando rebase aqui no branch título")
 for i in range(0,3):
     valor += float(input("digite um valor"))
 media = valor/3
