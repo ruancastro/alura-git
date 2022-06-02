5c48011a3f53cc42a6d54cc41c553f846bbe843c
Ruan Castro <ruancastro15@gmail.com>
2022-06-02 11:15:50 -0300

    média com M maiúsculo

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ print("Esse código é para calcular a média de três valores diferentes")
 for i in range(0,3):
     valor += float(input("digite um valor"))
 media = valor/3
-print("A média entre os três valores é: ",media)
+print("A Média entre os três valores é: ",media)
