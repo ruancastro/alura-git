59fbb00bffd56bc6dc3b8734fb6838d4a31b6ade
Ruan Castro <ruancastro15@gmail.com>
2022-06-02 11:14:54 -0300

    codigo mais enxuto no titulo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print("esse print só tem no título")
-
 valor = 0
-print("treinando rebase aqui no branch título")
+print("Esse código é para calcular a média de três valores diferentes")
 for i in range(0,3):
     valor += float(input("digite um valor"))
 media = valor/3
