5c8f28b63a4ee23fd066258a79e0aef1b216bc6d
Ruan Castro <ruancastro15@gmail.com>
2022-06-01 21:01:23 -0300

    corrigindo o valor inicial da variável valor

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+<<<<<<< HEAD
 valor = 0
 print("Esse código é para calcular a média de três valores diferentes")
+=======
+valor = 0.0
+>>>>>>> fc91cb6 (corrigindo o valor inicial da variável valor)
 for i in range(0,3):
     valor += float(input("digite um valor"))
 media = valor/3
