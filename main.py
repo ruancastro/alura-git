7a0029173677078eccff15d7a7d32cf4437692ae
Ruan Castro <ruancastro15@gmail.com>
2022-06-02 12:42:01 -0300

    digite maiusculo

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ valor = 0
 print("Esse código é para calcular a média de três valores diferentes")
 valor = 0
 for i in range(0,3):
-    valor += float(input("digite um valor"))
+    valor += float(input("DIGITE um valor"))
 media = valor/3
 print("A Média Entre os três valores é: ",media)
