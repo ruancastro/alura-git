c17f8b88b3f6d8c48087fd9021cf519308d011ca
Ruan Castro <ruancastro15@gmail.com>
2022-06-02 12:39:03 -0300

    letra e maiúscula no print final

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 valor = 0
 print("Esse código é para calcular a média de três valores diferentes")
-=======
-valor = 0.0
->>>>>>> fc91cb6 (corrigindo o valor inicial da variável valor)
-=======
-print("linha a mais no master")
 valor = 0
->>>>>>> b6c277b (colocando uma linha a mais no incio)
 for i in range(0,3):
     valor += float(input("digite um valor"))
 media = valor/3
-print("A Média entre os três valores é: ",media)
+print("A Média Entre os três valores é: ",media)
